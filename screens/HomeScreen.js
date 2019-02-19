60b8160e0f78782b8864e9fb7937e5e28f70a2c9
senalidee <47751129+senalidee@users.noreply.github.com>
2019-02-19 15:28:59 +0530

    Test Commit to check master push disabling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -52,7 +52,7 @@ export default class HomeScreen extends React.Component {
               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
             </TouchableOpacity>
           </View>
-          {/* Button Added */}
+          {/* Button Added and Modified*/}
           <Button
             onPress={console.log("Pressed")}
             title="Learn More"
